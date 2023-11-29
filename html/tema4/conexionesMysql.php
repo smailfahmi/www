aa27790e4211b243eeb531413062d0392c6cc2da
”Smail” <smail.fahcha@educa.jcyl.es>
2023-11-29 17:30:15 +0000

    i/2911-1830

---
+++ html/tema4/conexionesMysql.php
@@ -0,0 +1,34 @@
+<?
+require('./confiBD.php');
+try {
+    $con = mysqli_connect(IP, USER, PASS, 'prueba');
+    echo 'conectado';
+    $rnombre = 'miguel';
+    $redad = 30;
+    //$sql = "insert into alumnos (nombre,edad) values ('$rnombre','$redad')";
+    //consultas preparadas 
+    $sql = "insert into alumnos (nombre,edad) values (?,?)";
+    $stmt = mysqli_prepare($con, $sql);
+    mysqli_stmt_bind_param($stmt,"si", $rnombre, $redad);
+
+    mysqli_stmt_execute($stmt);
+    if (!mysqli_query($con, $sql)) {
+        echo mysqli_errno($con);
+    }
+    mysqli_close($con);
+} catch (\Throwable $th) {
+    switch ($th->code) {
+        case '1062':
+            echo 'id repetido';
+            break;
+
+        default:
+            echo $th->getMessage();
+            break;
+    }
+    // echo mysqli_errno($con);
+    // echo mysqli_connect_errno();
+    // echo mysqli_connect_error();
+    //probamos un par de errores y los guardamos en el switch
+    mysqli_close($con);
+}
---
+++ html/tema4/confiBD.php
@@ -0,0 +1,4 @@
+<?
+define('IP', '192.168.7.205');
+define('USER', 'smail');
+define('PASS', 'smail');
